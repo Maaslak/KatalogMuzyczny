279895cdf8c39bdaf5819a57c7c109166e783e15
Maaslak <bartekn1996@wp.pl>
2018-01-30 21:43:55 +0100

    Getting zespoly added.

---
+++ src/JavaObjects/Zespol.java
@@ -0,0 +1,53 @@
+package JavaObjects;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class Zespol {
+    private String nazwa, miasto_zalozenia, kraj_zalozenia;
+    private Date date;
+
+    public Zespol(String nazwa, Date date, String miasto_zalozenia, String kraj_zalozenia){
+        this.nazwa = nazwa;
+        this.date = date;
+        this.miasto_zalozenia = miasto_zalozenia;
+        this.kraj_zalozenia = kraj_zalozenia;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getMiasto_zalozenia() {
+        return miasto_zalozenia;
+    }
+
+    public void setMiasto_zalozenia(String miasto_zalozenia) {
+        this.miasto_zalozenia = miasto_zalozenia;
+    }
+
+    public String getKraj_zalozenia() {
+        return kraj_zalozenia;
+    }
+
+    public void setKraj_zalozenia(String kraj_zalozenia) {
+        this.kraj_zalozenia = kraj_zalozenia;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Nazwa " + nazwa + "\nData " + date.toString() + "\nMiasto zalozenia " + miasto_zalozenia + "\nKraj zalozenia " + kraj_zalozenia + "\n\n";
+    }
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,19 +2,35 @@ package Main;
 
 import DataBase.DataBaseConnector;
 import Gui.MainWindow;
+import JavaObjects.Zespol;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Main {
     private static DataBaseConnector dataBaseConnector;
 
+    public static void testZespoly(){
+        ArrayList<Zespol> zespoly = null;
+        try {
+            zespoly = dataBaseConnector.getZespoly();
+            for (Zespol zespol:
+                    zespoly) {
+                System.out.println(zespol);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String [] args) {
-        DataBaseConnector.setUser("maaslak");
-        DataBaseConnector.setPassword("maaslak");
+        DataBaseConnector.setUser("MAASLAK");
+        DataBaseConnector.setPassword("MAASLAK");
         try {
             dataBaseConnector = new DataBaseConnector();
             MainWindow mainWindow = new MainWindow();
+            testZespoly();
         } catch (SQLException e) {
             System.out.println("Nie udalo sie polaczyc z baza danych");
             e.printStackTrace();
